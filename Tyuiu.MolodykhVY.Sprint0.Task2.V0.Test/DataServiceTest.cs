d1ac237220868e4cbe6a6a45698807198516336d
MolodykhVlad <mr.prins@yandex.ru>
2023-09-16 14:44:31 +0300

    Task2

---
+++ Tyuiu.MolodykhVY.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.MolodykhVY.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MolodykhVY.Sprint0.Task2.V0.Test
+
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekGetMassageValid()
+        {
+
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Влад";
+            var res = DataService.GetMassage(name);
+
+            // вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Влад", res);
+
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.MolodykhVY.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MolodykhVY.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MolodykhVY.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DataService.GetMassage("Влад"));
+            Console.ReadKey();
+        }
+    }
+}
