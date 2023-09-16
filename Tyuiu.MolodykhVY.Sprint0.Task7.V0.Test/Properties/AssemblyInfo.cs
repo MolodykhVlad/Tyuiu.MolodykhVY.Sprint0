5c9790cdde23a6824b8eaec75d1c633ec3482c4d
MolodykhVlad <mr.prins@yandex.ru>
2023-09-16 19:31:56 +0300

    Task7

---
+++ Tyuiu.MolodykhVY.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MolodykhVY.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MolodykhVY.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("20c2a878-dfcd-40c6-a6b7-50b277e86811")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
